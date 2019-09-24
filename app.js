d2ceccc9f0aa9aced24a9a2b4e9c0b04f84dac2a
Anatoly <asavchenko.exceedteam@gmail.com>
2019-09-24 11:52:06 +0300

    update

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const todoRouter = require('./routes/todos');
+const connectDB = require('./database/db');
 const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/todo', todoRouter);
-app.use( (req, res, next) => {
+app.use((req, res, next) => {
 	next(404);
 });
 
-app.use((err, req, res ) => {
+app.use((err, req, res) => {
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
